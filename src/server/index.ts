54e89a7507114fcf113a24ff5af98cdb2c184f8d
Santiago Torres <ethx42@gmail.com>
2025-12-24 19:55:51 -0500

    feat(server): add barrel exports for server module
    Complete server module exports:
    - createApp: Fastify application factory
    - ServerConfig, ServerDependencies: Configuration types
    - registerEventsRoute: SSE endpoint registration
    - registerDashboardRoute: Dashboard HTML serving
    
    This completes Milestone 4: Observability Layer

---
+++ src/server/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Server Module Barrel Export
+ *
+ * @module server
+ */
+
+export { type ServerConfig, type ServerDependencies, createApp } from "./app.js";
+export { registerEventsRoute, registerDashboardRoute } from "./routes/index.js";
+
---
+++ src/server/routes/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Routes Barrel Export
+ *
+ * @module server/routes
+ */
+
+export { registerEventsRoute } from "./events.route.js";
+export { registerDashboardRoute } from "./dashboard.route.js";
+
